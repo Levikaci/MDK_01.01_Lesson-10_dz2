6d92a14c76069561cd6f796d800345d00ebaea71
Levikaci <121298029+Levikaci@users.noreply.github.com>
2025-01-27 18:48:52 +0300

    Добавьте файлы проекта.

---
+++ dz2/Program.cs
@@ -0,0 +1,26 @@
+﻿public abstract class Translator
+{
+    public abstract string GetLanguage();
+}
+
+public class EnglishTranslator : Translator
+{
+    public override string GetLanguage()
+    {
+        return "Английский"; 
+    }
+
+    public EnglishTranslator()
+    {
+        Console.WriteLine("Я переводчик с английского");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        EnglishTranslator translator = new EnglishTranslator();
+       
+    }
+}
